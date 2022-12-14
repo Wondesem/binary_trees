ef35eda6630ac165afc5f20751f4749c4c90684c
wondesem <wseminew50gmail.com>
2022-12-14 05:33:10 -0800

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+# include "binary_trees.h"
+/**
+ * binary_tree_t - creates a binary tree
+ * @parent: pointer to parent
+ * @value: value to be placed in newnode
+ * Return: pointer to the new node or NULL if failed
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+{
+	binary_tree_t *newnode;
+
+	newnode = maloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
+}
