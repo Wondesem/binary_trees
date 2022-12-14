c384dee303da2ed86b9a20b89efb1124bcb0cf00
wondesem <wseminew50gmail.com>
2022-12-14 05:35:04 -0800

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 {
 	binary_tree_t *newnode;
 
-	newnode = maloc(sizeof(binary_tree_t));
+	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = value;
