667280b77a8e2bd6a258f94f9ffef22dd8ceaeca
wondesem <wseminew50gmail.com>
2022-12-15 02:54:32 -0800

    task 10 and 11

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,13 +6,13 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
-	if (tree)
+	size_t depth = 0;
+	if (tree == NULL)
+		return (0);
+	while (tree->parent)
 	{
-		size += 1;
-		size += binary_tree_size(tree->left);
-		size += binary_tree_size(tree->right);
+		depth++;
+		tree = tree->parent;
 	}
-	return (size);
+	return (depth);
 }
