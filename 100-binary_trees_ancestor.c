0d9055b173630f4183d53436113ed7b4aa033758
wondesem <wseminew50gmail.com>
2022-12-15 05:09:21 -0800

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,21 +9,20 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *node;
-	
-	if (first == NULL || second == NULL)
-		return (NULL);
-	node = (binary_tree_t *)second;
-	while (first != NULL)
-	{
-		while (second)
-		{
-			if (first == second)
-				return ((binary_tree_t *)first);
-			second = second->parent;
-		}
-		second = node;
-		first = first->parent;
-	}
+binary_tree_t *node;
+if (first == NULL || second == NULL)
 	return (NULL);
+node = (binary_tree_t *)second;
+while (first != NULL)
+{
+	while (second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		second = second->parent;
+	}
+	second = node;
+	first = first->parent;
+}
+return (NULL);
 }
